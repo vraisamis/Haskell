45e4bac7bee27bd4e4a62397f362ddad7cdb31a7
vraisamis <quick.sorting.arg@gmail.com>
2014-10-15 15:37:15 +0900

    solve problem 005

--- ProjectEuler/0xx.hs
+++ ProjectEuler/0xx.hs
@@ -3,7 +3,10 @@ module ProjectEuler0xx
 , problem002
 , problem003
 , problem004
+, problem005
 ) where
+
+import Data.List
 
 problem001 :: Int
 problem001 = foldl (+) 0 xs
@@ -21,4 +24,8 @@ problem003 = foldl max 1 [x | x <- [1..n], n `mod` x == 0]
 
 problem004 :: Integer
 problem004 = head [x*y | x <- reverse [100..999], y <- reverse [100..999], (reverse $ show (x*y)) == (show (x*y))]
+
+problem005 :: Integer
+problem005 = foldl lcm 1 [2..20]
+    where lcm n m = (n * m) `div` (gcd n m)
 
